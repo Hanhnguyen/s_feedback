83d3c1cad340ab34b8e367d388213a2048fb22f8
Hanh Nguyen <hanhn13@stanford.edu>
2013-08-20 13:12:57 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SealnetFeedback::Application.config.secret_token = 'f4387339821d1ef629fde1cc3c04844d84328ffc19d35cc3defa8166d76ccd6d3f6bdacb11255d8c199e7f8ef8bd3b747017096636c2d9ff9d0ddd5942e52c5d'
